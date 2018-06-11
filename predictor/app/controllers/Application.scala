b8f184b0cb9d65191c34ae86c825d7dd7777aaef
deniskovalenko <denis.v.kovalenko@gmail.com>
2018-06-11 11:08:47 +0300

    fix API token and project check

--- predictor/app/controllers/Application.scala
+++ predictor/app/controllers/Application.scala
@@ -7,6 +7,7 @@ import play.api.Configuration
 import play.api.cache.AsyncCacheApi
 import play.api.libs.json.Json
 import play.api.mvc.{AbstractController, ControllerComponents}
+import play.mvc.Result
 import play.mvc.Results._
 import service.ScoringService
 
@@ -22,22 +23,25 @@ class Application @Inject()(cc: ControllerComponents,
   val allowedTopics = conf.get[String]("api.allowed_topics").split(":")
 
 
-  def predict(project: String, api_token: String, comment: String) = Action.async {
-    //    todo fix returned response if API token is incorrect or project isn't one of allowed
-    if (api_token != api_token) {
-      Future.successful(unauthorized("api_token is incorrect"))
-    } else if (!allowedTopics.contains(project)) {
-      Future.successful(badRequest("supplied project " + project + " isn't registered in the system"))
-    }
-
+  def predict(project: String, api_token: String, comment: String)  = Action.async {
     val promise = Promise[String]
-    scoringService.requestPrediction(project, comment, promise)
-    //this promise is fullfilled in Kafka Consumer when response from python model is obtained
-    promise.future.map(result => Ok(result))
+    if (apiToken != api_token) {
+      promise.success("api_token is incorrect")
+      promise.future.map(result => Unauthorized(result))
+    } else if (!allowedTopics.contains(project)) {
+      val promise = Promise[String]
+      promise.success("supplied project " + project + " isn't registered in the system")//Unauthorized("api_token is incorrect"))
+      promise.future.map(result => BadRequest(result))
+    }
+    else {
+      val promise = Promise[String]
+      scoringService.requestPrediction(project, comment, promise)
+      //this promise is fullfilled in Kafka Consumer when response from python model is obtained
+      promise.future.map(result => Ok(result))
+    }
   }
 
   def metrics(project: String) = Action {
-    //    val db = RocksDB.open(options, "/tmp/kafka")
     Ok(Json.obj("metrics" -> Json.toJson(GlobalState.metrics)))
   }
 
